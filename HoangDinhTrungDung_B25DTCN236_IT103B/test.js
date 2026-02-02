3ca2659c175bcdbcff844879b3c58088721f2f1c
HoangDung34 <hngdungtr55@gmail.com>
2026-02-02 13:35:04 +0700

    Add files via upload

---
+++ HoangDinhTrungDung_B25DTCN236_IT103B/test.js
@@ -0,0 +1,24 @@
+let n = Number(prompt("Nhập một số nguyên n : "));
+    console.log("Nhập số n :", n);
+
+    if(n % 2 == 0){
+        console.log("số là số chẵn"); 
+    }else{
+        console.log("Là số lẻ");   
+    }
+
+    if(n == 0){
+        console.log("n = 0")
+    }else if(n > 0){
+        console.log("Đây là số dương");
+    }else{
+        console.log("Đây là số âm");
+    }
+
+    for(i = 1; i <= n; i++){
+        if(n > 0){
+            console.log(`${i}`);
+        }else if(n <= 0){
+            console.log("giá trị không hợp lệ")
+        }
+    }
